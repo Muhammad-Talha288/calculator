a7e22be3710593756ee23e809f662d60b5cea657
Muhammad-Talha288 <talhamengal288@gmail.com>
2023-04-05 22:11:25 +0500

    There is my first calc master file

---
+++ index.ts
@@ -0,0 +1,46 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+let clc = await inquirer.prompt([
+  {
+    name: "num1",
+    message: "Enter any number",
+    type: "number",
+  },
+  {
+    name: "num2",
+    message: "Enter any number",
+    type: "number",
+  },
+  {
+    name: "operator",
+    message: "Enter any operator + , - , / , * , % , **",
+    type: "string",
+  },
+]);
+
+if (clc.num1 && clc.num2 && clc.operator === "+") {
+  let result = clc.num1 + clc.num2;
+  console.log(result);
+} else if (clc.num1 && clc.num2 && clc.operator === "-") {
+  let result = clc.num1 - clc.num2;
+  console.log(chalk.green(result));
+} else if (clc.num1 && clc.num2 && clc.operator === "*") {
+  let result = clc.num1 * clc.num2;
+  console.log(chalk.green(result));
+} else if (clc.num1 && clc.num2 && clc.operator === "/") {
+  let result = clc.num1 / clc.num2;
+  console.log(chalk.green(result));
+} else if (clc.num1 && clc.num2 && clc.operator === "%") {
+  let result = clc.num1 % clc.num2;
+  console.log(chalk.green(result));
+} else if (clc.num1 && clc.num2 && clc.operator === "**") {
+  let result = clc.num1 ** clc.num2;
+  console.log(result);
+} else {
+  console.log(
+    chalk.red("Please Select only these Operators '+ , - , * , / , % ' ")
+  );
+}
